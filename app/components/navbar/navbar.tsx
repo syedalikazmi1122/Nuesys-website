123b03f603f7a886a44eb11bd07cfa10d63b869a
syedalikazmi1122 <syedalikazmi0012@gmail.com>
2024-02-14 17:22:05 +0500

    images rectfied

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -7,7 +7,12 @@ export default function Navbar() {
       <div className="flex content-between  w-screen items-center justify-center">
         <div className="flex items-center p-4  justify-center">
           {/* The icon of the site */}
-          <Image alt="nyesyslogo" src={"/navbarpics/nuesyslogo"} width={100} height={100} />
+          <Image
+            alt="nyesyslogo"
+            src={"/navbarpics/nuesyslogo.png"}
+            width={100}
+            height={100}
+          />
           <div className=" ml-48 flex space-x-5">
             {/* the links for pages */}
             <NavLink title="Home" link="./home" />
